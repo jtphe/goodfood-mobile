4e6c0c6a7c363688c3f7895d0ddef3a15eed91ad
Jean Phe <jean.phe.fr@gmail.com>
2022-02-26 18:02:45 +0100

    feat(add-splashscreen): add app splashscreen

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreenAnimated from '@shared/SplashScreenAnimated';
 import Home from '@components/Home';
 import i18n from '@i18n/i18n';
 
-function App() {
+const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={i18n.t('home.screenTitle')} component={Home} />
-      </Stack.Navigator>
+      <SplashScreenAnimated>
+        <Stack.Navigator>
+          <Stack.Screen name={i18n.t('home.screenTitle')} component={Home} />
+        </Stack.Navigator>
+      </SplashScreenAnimated>
     </NavigationContainer>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
 /**
@@ -7,24 +7,24 @@ import PropTypes from 'prop-types';
  * @param {Object} navigation - Props used to navigate between screens
  */
 const Home = ({navigation}) => {
-    console.log('navigation',  navigation)
-    return (
-        <View style={styles.container}>
-            <Text>Hello world</Text>
-        </View>
-    )
-}
+  console.log('navigation', navigation);
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-container : {
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-}
-})
+  },
+});
 
 Home.propTypes = {
-    navigation: PropTypes.object
-}
+  navigation: PropTypes.object
+};
 
 export default Home;
